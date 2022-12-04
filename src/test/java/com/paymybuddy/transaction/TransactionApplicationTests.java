2aba2b636357aca09355530c2aba0c9e8e71a4e3
dimitraye <dimitraye94@gmail.com>
2022-12-04 16:48:27 +0100

    first commit

---
+++ src/test/java/com/paymybuddy/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
